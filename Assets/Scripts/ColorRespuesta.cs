2333e64b701cd8c60be3b1ef59af606888c12665
zPopeyex <stven-457@hotmail.com>
2021-11-18 01:02:51 -0500

    first commit

---
+++ Assets/Scripts/ColorRespuesta.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class ColorRespuesta : MonoBehaviour
+{
+    public static RawImage NumeroRespuestaColor;
+    public RawImage ImagenRespuesta1;
+    public RawImage ImagenRespuesta2;
+    public RawImage ImagenRespuesta3;
+    public RawImage ImagenRespuesta4;
+    public Texture2D Verde;
+    public Texture2D Rojo;
+    public Texture2D Blanco;
+
+    public static int NumeroBoton = 0;
+
+    public void ObtenerNumeroBoton(int Numero)
+    {
+   
+        NumeroBoton = Numero;
+
+        if (NumeroBoton == 1)
+        {
+            NumeroRespuestaColor = ImagenRespuesta1;
+            ImagenRespuesta2.texture = Blanco;
+            ImagenRespuesta3.texture = Blanco;
+            ImagenRespuesta4.texture = Blanco;
+        }
+        if (NumeroBoton == 2)
+        {
+            NumeroRespuestaColor = ImagenRespuesta2;
+            ImagenRespuesta1.texture = Blanco;
+            ImagenRespuesta3.texture = Blanco;
+            ImagenRespuesta4.texture = Blanco;
+        }
+        if (NumeroBoton == 3)
+        {
+            NumeroRespuestaColor = ImagenRespuesta3;
+            ImagenRespuesta1.texture = Blanco;
+            ImagenRespuesta2.texture = Blanco;
+            ImagenRespuesta4.texture = Blanco;
+        }
+        if (NumeroBoton == 4)
+        {
+            NumeroRespuestaColor = ImagenRespuesta4;
+            ImagenRespuesta1.texture = Blanco;
+            ImagenRespuesta2.texture = Blanco;
+            ImagenRespuesta3.texture = Blanco;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Principal.PreguntasPlazoleta[0].correcta == NumeroBoton)
+        {
+            NumeroRespuestaColor.texture = Verde;
+
+        }
+        if (NumeroBoton > 0 && NumeroBoton != Principal.PreguntasPlazoleta[0].correcta)
+        {
+            NumeroRespuestaColor.texture = Rojo;
+
+        }
+        if (NumeroBoton == 0)
+        {
+            NumeroRespuestaColor.texture = Blanco;
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Principal.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Principal : MonoBehaviour
+{
+    public class Pregunta
+    {
+        public string pregunta;
+        public string Respuesta1;
+        public string Respuesta2;
+        public string Respuesta3;
+        public string Respuesta4;
+        //correcta: es el numero de respuesta que esta correcta
+        public int correcta;
+        public bool aprobada;
+
+        public Pregunta(string pregunta, string Respuesta1, string Respuesta2, string Respuesta3, string Respuesta4, int correcta, bool aprobada)
+        {
+            this.pregunta = pregunta;
+            this.Respuesta1 = Respuesta1;
+            this.Respuesta2 = Respuesta2;
+            this.Respuesta3 = Respuesta3;
+            this.Respuesta4 = Respuesta4;
+            this.correcta = correcta;
+            this.aprobada = aprobada;
+        }
+    }
+
+    public Text TextoPregunta;
+    public Text TextoRespuestas1;
+    public Text TextoRespuestas2;
+    public Text TextoRespuestas3;
+    public Text TextoRespuestas4;
+
+    public static int NPreguntasPlazoleta = 1;
+    public static Pregunta[] PreguntasPlazoleta = new Pregunta[NPreguntasPlazoleta];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PreguntasPlazoleta[0] = new Pregunta("¿Cuntos dedos tiene un humano?\n(Ejemplo)", "4", "10", "20", "5", 3, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextoPregunta.text = PreguntasPlazoleta[0].pregunta;
+        TextoRespuestas1.text = PreguntasPlazoleta[0].Respuesta1;
+        TextoRespuestas2.text = PreguntasPlazoleta[0].Respuesta2;
+        TextoRespuestas3.text = PreguntasPlazoleta[0].Respuesta3;
+        TextoRespuestas4.text = PreguntasPlazoleta[0].Respuesta4;
+
+    }
+}
